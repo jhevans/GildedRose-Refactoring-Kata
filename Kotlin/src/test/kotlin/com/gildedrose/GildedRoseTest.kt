3d04d26e9d7c9a4ff436a1415d09f78f3798bc0d
John Evans <john.evans@digital.justice.gov.uk>
2023-01-29 16:42:10 +0000

    ✅ Add tests for Aged Brie

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -18,8 +18,10 @@ internal class GildedRoseTest {
   }
 
   @Nested
-  @DisplayName(value="Given a standard item")
+  @DisplayName(value = "Given a standard item")
   inner class StandardItemTest {
+    // TODO: This doesn't cover a large range of item names, possibly missed a trick testing this way, may be worth refactoring
+    // Might be better to refactor these tests to be behaviour-centric rather than item-centric 🤔
 
     @Test
     fun `Then sellIn and quality are reduced by one`() {
@@ -30,12 +32,13 @@ internal class GildedRoseTest {
     }
 
     @ParameterizedTest(name = "quality of {0} is reduced by 2")
-    @ValueSource(ints = [0,-1, -999, Int.MIN_VALUE+1])
+    @ValueSource(ints = [0, -1, -999, Int.MIN_VALUE + 1])
     fun `And a sellIn of 0 or less, then quality is reduced by 2`(startingSellIn: Int) {
       val items = arrayOf(Item("Thing", startingSellIn, 20))
       val app = GildedRose(items)
       app.updateQuality()
-      assertThat(app.items[0]).usingRecursiveComparison().isEqualTo(Item("Thing", sellIn = startingSellIn-1, quality = 18))
+      assertThat(app.items[0]).usingRecursiveComparison()
+        .isEqualTo(Item("Thing", sellIn = startingSellIn - 1, quality = 18))
     }
 
     @ParameterizedTest(name = "quality of {0} is reduced to 0")
@@ -63,15 +66,45 @@ internal class GildedRoseTest {
   }
 
   @Nested
+  @DisplayName(value = "Given an aged Brie")
+  inner class AgedBrieTest {
+    @ParameterizedTest(name = "quality of {0} is increased by 1")
+    @ValueSource(ints = [0, 1, 5, 49])
+    fun `Then sellIn is reduced by one, quality increases by one`(startingQuality: Int) {
+      val items = arrayOf(Item("Aged Brie", 10, startingQuality))
+      val app = GildedRose(items)
+      app.updateQuality()
+      assertThat(app.items[0]).usingRecursiveComparison().isEqualTo(Item("Aged Brie", sellIn = 9, quality = startingQuality + 1))
+    }
+
+    @Test
+    fun `And a quality of 50, then quality stays at 50`() {
+      val items = arrayOf(Item("Aged Brie", 10, 50))
+      val app = GildedRose(items)
+      app.updateQuality()
+      assertThat(app.items[0]).usingRecursiveComparison().isEqualTo(Item("Aged Brie", sellIn = 9, quality = 50))
+    }
+  }
+
+  @Nested
   inner class UndefinedBehaviourTest {
     @Test
     fun `Given input has a negative value for quality then return the value unchanged`() {
-      // According to the spec this can't happen but there's nothing preventing instantiation of an Item with a negative
-      // quality, in this case it just returns the quality unchanged
       val items = arrayOf(Item("Thing", 0, -1))
       val app = GildedRose(items)
       app.updateQuality()
       assertThat(app.items[0]).usingRecursiveComparison().isEqualTo(Item("Thing", sellIn = -1, quality = -1))
+    }
+
+    @ParameterizedTest
+    @DisplayName(value = "Given {0} then quality is 51")
+    @CsvSource(delimiter = '|', value = ["Aged Brie|51", "Thing|50", "Backstage passes to a TAFKAL80ETC concert|51"])
+    // TODO: This test may be redundant once everything else is tested
+    fun `Given input has a quality greater than 50 then return some value`(itemName: String, itemQuality: Int) {
+      val items = arrayOf(Item(itemName, 10, 51))
+      val app = GildedRose(items)
+      app.updateQuality()
+      assertThat(app.items[0]).usingRecursiveComparison().isEqualTo(Item(itemName, sellIn = 9, quality = itemQuality))
     }
   }
 }
